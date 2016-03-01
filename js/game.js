cef6707e589f1dc1b7aacc8e4c3958551465d54f
Alexander Yudin <true.yudin@gmail.com>
2016-03-01 12:15:25 +0300

    module6-task2
    complete

--- js/game.js
+++ js/game.js
@@ -238,6 +238,27 @@
     return state;
   };
 
+  var clouds = document.querySelector('.header-clouds');
+  var demoBlock = document.querySelector('.demo');
+  var pageScrollTimeout;
+  var canCloudsShift = true;
+  var CLOUDS_TIMEOUT = 100;
+
+  window.addEventListener('scroll', function() {
+    var cloudsOffset = clouds.getBoundingClientRect().height - window.scrollY;
+
+    if (canCloudsShift) {
+      clouds.style.backgroundPosition = cloudsOffset + 'px';
+    }
+    clearTimeout(pageScrollTimeout);
+    pageScrollTimeout = setTimeout(function() {
+      if (demoBlock.getBoundingClientRect().bottom <= 0) {
+        game.setGameStatus(window.Game.Verdict.PAUSE);
+      }
+      canCloudsShift = cloudsOffset > 0;
+    }, CLOUDS_TIMEOUT);
+  });
+
   /**
    * Конструктор объекта Game. Создает canvas, добавляет обработчики событий
    * и показывает приветственный экран.
