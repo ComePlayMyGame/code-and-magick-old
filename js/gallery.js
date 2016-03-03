28ad39f52e32f23ef3145d9669d4db9fbe7e2904
Alexander Yudin <true.yudin@gmail.com>
2016-03-03 17:44:22 +0300

    module7-task2

---
+++ js/gallery.js
@@ -0,0 +1,52 @@
+'use strict';
+
+(function() {
+  function Gallery() {
+    this.galleryOverlay = document.querySelector('.overlay-gallery');
+    this.galleryCloseButton = this.galleryOverlay.querySelector('.overlay-gallery-close');
+    this.leftControl = this.galleryOverlay.querySelector('.overlay-gallery-control-left');
+    this.rightControl = this.galleryOverlay.querySelector('.overlay-gallery-control-right');
+
+    this._onCloseClick = this._onCloseClick.bind(this);
+    this._onDocumentKeyDown = this._onDocumentKeyDown.bind(this);
+    this.leftControl = this.leftControl.bind(this);
+    this.rightControl = this.rightControl.bind(this);
+  }
+
+  Gallery.prototype.show = function() {
+    this.galleryOverlay.classList.remove('invisible');
+
+    this.galleryCloseButton.addEventListener('click', this._onCloseClick);
+    this.leftControl.addEventListener('click', this._onLeftClick);
+    this.rightControl.addEventListener('click', this._onRightClick);
+    document.addEventListener('keydown', this._onDocumentKeyDown);
+  };
+
+  Gallery.prototype.hide = function() {
+    this.galleryOverlay.classList.add('invisible');
+    this.galleryCloseButton.removeEventListener('click', this._onCloseClick);
+    this.leftControl.removeEventListener('click', this._onLeftClick);
+    this.rightControl.removeEventListener('click', this._onRightClick);
+    document.removeEventListener('keydown', this._onDocumentKeyDown);
+  };
+
+  Gallery.prototype._onCloseClick = function() {
+    this.hide();
+  };
+
+  Gallery.prototype._onLeftClick = function() {
+    console.log('клик по кнопке влево');
+  };
+
+  Gallery.prototype._onRightClick = function() {
+    console.log('клик по кнопке вправо');
+  };
+
+  Gallery.prototype._onDocumentKeyDown = function(evt) {
+    if (evt.keyCode === 27) {
+      this._onCloseClick();
+    }
+  };
+
+  window.Gallery = Gallery;
+})();
